5dd27064b7f3bbda2458f7cb7ccf6fb9eebb2800
Antoine Seweryn <antoine.seweryn@gmail.com>
2018-01-21 19:08:44 +0100

    JS Afficher/Masquer

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function validateForm() {
 	var email = document.getElementById("Email").value;
 	alert('Voulez-vous confirmer votre demande avec cette adresse mailm: ' + email);
    }
-function hideFormation(){
 
+function hide(id){
+	if(document.getElementById(id).className == "Masque"){
+	document.getElementById(id).className = "Display";
+	}else if(document.getElementById(id).className != "Masque"){
+	document.getElementById(id).className = "Masque";
+	}
 }
